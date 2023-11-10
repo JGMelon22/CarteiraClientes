8006b24dd6abdb894d108e6c0ef07feba8281b87
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-10 18:16:37 -0300

    chore: desenvolvendo a EntityConfig para ClientCompany (entidade intermediária)

---
+++ Infrastructure/Configuration/ClientCompanyConfiguration.cs
@@ -0,0 +1,52 @@
+using CarteiraClientes.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CarteiraClientes.Infrastructure.Configuration;
+
+public class ClientCompanyConfiguration : IEntityTypeConfiguration<ClientCompany>
+{
+    public void Configure(EntityTypeBuilder<ClientCompany> builder)
+    {
+        builder.ToTable("clients_companies");
+
+        builder.HasKey(cc => new
+        {
+            cc.ClientId,
+            cc.CompanyId
+        });
+
+        builder.HasIndex(cc => cc.ClientId)
+            .HasDatabaseName("client_company_client_id_idx");
+
+        builder.HasIndex(cc => cc.CompanyId)
+            .HasDatabaseName("client_company_company_id_idx");
+
+        // Explicit FK configuration 
+        builder.HasOne(cc => cc.Client)
+            .WithMany(c => c.ClientsCompanies)
+            .HasForeignKey(cc => cc.ClientId);
+
+        builder.Property(cc => cc.ClientId)
+            .HasColumnName("client_id")
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder.Property(cc => cc.CompanyId)
+            .HasColumnName("company_id")
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder.Property(cc => cc.Client)
+            .HasColumnType("varchar")
+            .HasMaxLength(100)
+            .HasColumnName("client_name")
+            .IsRequired();
+
+        builder.Property(cc => cc.Company)
+            .HasColumnType("varchar")
+            .HasMaxLength(100)
+            .HasColumnName("company_name")
+            .IsRequired();
+    }
+}
