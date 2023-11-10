f92cc22771c352a2944d9b67e648a861b385ad57
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-10 17:41:38 -0300

    chore: desenvolvendo a primeira parte do DbContext e registrando seu serviço

--- Infrastructure/Data/AppDbContext.cs
+++ Infrastructure/Data/AppDbContext.cs
@@ -1,6 +1,14 @@
+using CarteiraClientes.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace CarteiraClientes.Infrastructure.Data;
 
-public class AppDbContext 
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Client> Clients => Set<Client>();
+    public DbSet<Company> Companies => Set<Company>();
 }
