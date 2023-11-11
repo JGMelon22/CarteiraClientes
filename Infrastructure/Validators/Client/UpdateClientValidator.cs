b7414706e57c03031812efe1a3c1783929161017
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-11 12:21:39 -0300

    desenvolvendo validator para edição de cliente

---
+++ Infrastructure/Validators/Client/UpdateClientValidator.cs
@@ -0,0 +1,49 @@
+using CarteiraClientes.Models.Enums;
+using CarteiraClientes.ViewModels.Client;
+using FluentValidation;
+
+namespace CarteiraClientes.Infrastructure.Validators.Client;
+
+public class UpdateClientValidator : AbstractValidator<UpdateClientViewModel>
+{
+    public UpdateClientValidator()
+    {
+        RuleFor(c => c.ClientId)
+            .NotEmpty()
+            .WithMessage("Código do Cliente deve ser informado!")
+            .NotNull()
+            .WithMessage("Código do Cliente deve ser informado!");
+
+        RuleFor(c => c.FullName)
+            .NotEmpty()
+            .WithMessage("Nome do Cliente deve ser informado!")
+            .NotNull()
+            .WithMessage("Nome do Cliente deve ser informado!")
+            .MinimumLength(2)
+            .WithMessage("Nome do Cliente deve conter ao menos 2 letras!")
+            .MaximumLength(100)
+            .WithMessage("Nome do Cliente deve conter no máxima 100 letras!");
+
+        RuleFor(c => c.Age)
+            .NotEmpty()
+            .WithMessage("Idade do Cliente deve ser informada!")
+            .NotNull()
+            .WithMessage("Idade do Cliente deve ser informada!")
+            .Must(age => age >= 18)
+            .WithMessage("O Cliente deve ser maior de idade!");
+
+        RuleFor(c => c.Document)
+            .NotEmpty()
+            .WithMessage("Documento (RG) do Cliente deve ser informado!")
+            .NotNull()
+            .WithMessage("Documento (RG) do Cliente deve ser informado!");
+
+        RuleFor(c => c.Gender)
+            .NotEmpty()
+            .WithMessage("Sexo do Cliente deve ser informado")
+            .NotNull()
+            .WithMessage("Sexo do Cliente deve ser informado")
+            .Must(gender => gender == Gender.Female || gender == Gender.Male)
+            .WithMessage("Sexo do Cliente deve ser \"Female\" ou \"Male\"");
+    }
+}
