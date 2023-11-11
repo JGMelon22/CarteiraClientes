bde749e812abb2ae33074f4eb190ab4fcc5e934b
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-11 17:58:22 -0300

    feat: desenvolvendo controller base para testar client

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -1,4 +1,5 @@
 using CarteiraClientes.Interfaces;
+using CarteiraClientes.ViewModels.Client;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarteiraClientes.Controllers;
@@ -21,5 +22,49 @@ public class ClientsController : ControllerBase
         return clients.Data != null
             ? Ok(clients)
             : NoContent();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Details(int id)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var client = await _repository.GetClientById(id);
+        return client.Data != null
+            ? Ok(client)
+            : NotFound();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(AddClientViewModel newClient)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        await _repository.AddClient(newClient);
+        return Ok();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Edit(UpdateClientViewModel updatedClient)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var client = await _repository.UpdateClient(updatedClient);
+        return client.Data != null
+            ? Ok(client)
+            : NotFound(client);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        await _repository.RemoveClient(id);
+        return NoContent();
     }
 }
