c9bfb2821d8e2ceb254e1fb46c5cd58f6fd6ec12
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-11 12:46:28 -0300

    chore: desenvolvendo validator para adição e edição de company

---
+++ Infrastructure/Validators/Company/UpdateCompanyValidator.cs
@@ -0,0 +1,48 @@
+using CarteiraClientes.ViewModels.Company;
+using FluentValidation;
+
+namespace CarteiraClientes.Infrastructure.Validators.Company;
+
+public class UpdateCompanyValidator : AbstractValidator<UpdateCompanyViewModel>
+{
+    public UpdateCompanyValidator()
+    {
+        // Validar data 
+        DateTime dataValida = DateTime.Now;
+
+        // Minimum gross revenue
+        decimal minimunGrossRevenue = 100.00M;
+
+        RuleFor(c => c.CompanyId)
+            .NotEmpty()
+            .WithMessage("Código comp")
+            .WithMessage("Código do Cliente deve ser informado!")
+            .NotNull()
+            .WithMessage("Código do Cliente deve ser informado!");
+
+        RuleFor(c => c.CompanyName)
+            .NotEmpty()
+            .WithMessage("Nome da empresa deve ser informado!")
+            .NotNull()
+            .WithMessage("Nome da empresa deve ser informado!")
+            .MinimumLength(2)
+            .WithMessage("Nome da empresa deve conter ao menos 2 caracteres")
+            .MaximumLength(100)
+            .WithMessage("Nome da empresa não deve exceder 100 caracteres");
+
+        RuleFor(c => c.FoundedDate)
+            .NotEmpty()
+            .WithMessage("Data de criação da empresa deve ser informada!")
+            .NotNull()
+            .WithMessage("Data de criação da empresa deve ser informada!")
+            .Must(foundedDate => foundedDate > dataValida);
+
+        RuleFor(c => c.Revenue)
+            .NotEmpty()
+            .WithMessage("Receita bruta da empresa deve ser informada!")
+            .NotNull()
+            .WithMessage("Receita bruta da empresa deve ser informada!")
+            .Must(revenue => revenue > minimunGrossRevenue)
+            .WithMessage($"Receita bruta da empresa deve ser superior a {minimunGrossRevenue:C2}");
+    }
+}
