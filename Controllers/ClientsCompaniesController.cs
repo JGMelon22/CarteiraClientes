219ec2f9dec55462ad32455d91c614ef5ae1aa5d
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-15 15:36:46 -0300

    chore: desenvolvendo ClientCompany controller

---
+++ Controllers/ClientsCompaniesController.cs
@@ -0,0 +1,23 @@
+using CarteiraClientes.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarteiraClientes.Controllers;
+
+public class ClientsCompaniesController : Controller
+{
+    private readonly IClientCompanyRepository _repository;
+
+    public ClientsCompaniesController(IClientCompanyRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var clientsCompanies = await _repository.GetAllClientsCompanies();
+        return clientsCompanies.Data != null
+            ? Ok(View(clientsCompanies.Data))
+            : NoContent();
+    }
+}
