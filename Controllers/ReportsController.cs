d7718be195ca4cbe10cb0e8cdb3786f89a0e2825
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-20 13:19:08 -0300

    chore: desenvolvimento inicial da Controller para Reports

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+using CarteiraClientes.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarteiraClientes.Controllers;
+
+public class ReportsController : Controller
+{
+    private readonly IReportService _service;
+
+    public ReportsController(IReportService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var reports = _service.GetAllReports();
+        return View(reports);
+    }
+    
+    
+}
