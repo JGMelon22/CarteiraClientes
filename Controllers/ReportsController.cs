c47bb7a2899b615021b05c5f78b16e4b0554f2cc
JGMelon22 <jgvasconcellos22@gmail.com>
2023-11-20 14:30:15 -0300

    chore: desenvolvendo os métodos de download e deleção de arquivos na report controller

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -24,4 +24,18 @@ public class ReportsController : Controller
         await _service.PlotReport();
         return RedirectToAction("Index", "Reports");
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Download(string reportName)
+    {
+        byte[] bytes = await _service.DownloadReport(reportName); // bytes array to invoke file download service
+        return File(bytes, "application/octet-stream", reportName); // generic binary file type
+    }
+
+    [HttpGet]
+    public IActionResult Delete(string reportName)
+    {
+        _service.DeleteReport(reportName);
+        return RedirectToAction("Index", "Reports");
+    }
 }
