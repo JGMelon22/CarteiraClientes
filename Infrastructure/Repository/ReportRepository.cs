2507fe13c28bdd7df57a22ce3148e1c7b3a1b5bb
JGMelon22 <jgvasconcellos22@gmail.com>
2023-12-08 13:00:23 -0300

    chore: updating IReportRepository to use GetAllClientsCompaniesEfCoreRawQueryUnmappedType and appling it's new contract at ReportRepository

--- Infrastructure/Repository/ReportRepository.cs
+++ Infrastructure/Repository/ReportRepository.cs
@@ -1,4 +1,4 @@
-using CarteiraClientes.ViewModels.ClientCompany;
+using CarteiraClientes.ViewModels.Report;
 
 namespace CarteiraClientes.Infrastructure.Repository;
 
@@ -11,25 +11,34 @@ public class ReportRepository : IReportRepository
         _dbContext = dbContext;
     }
 
-    public async Task<ServiceResponse<List<GetClientCompanyViewModel>>> GetAllClientsCompanies()
+    /// <summary>
+    ///     Performs an Inner Join operation with RawQuery for Unmapped Type
+    /// </summary>
+    /// <returns>Returns same data as traditional EF Core approach</returns>
+    public async Task<ServiceResponse<List<GetReportViewModel>>> GetAllClientsCompaniesEfCoreRawQueryUnmappedType()
     {
-        var serviceResponse = new ServiceResponse<List<GetClientCompanyViewModel>>();
+        var serviceResponse = new ServiceResponse<List<GetReportViewModel>>();
 
-        var clientsCompanies = await (from cl in _dbContext.Clients
-                                      join cc in _dbContext.ClientsCompanies on cl.ClientId equals cc.ClientId
-                                      join co in _dbContext.Companies on cc.CompanyId equals co.CompanyId
-                                      select new
-                                      {
-                                          cl.ClientId,
-                                          cl.FullName,
-                                          cl.Document,
-                                          cl.IsOverdue,
-                                          co.CompanyId,
-                                          co.CompanyName
-                                      }).AsNoTracking()
+        var clientsCompanies = await _dbContext
+            .Database
+            .SqlQueryRaw<GetReportViewModel>(@"
+                   SELECT c.client_id AS ClientId, 
+                          c.full_name AS FullName, 
+                          c.document AS Document, 
+                          c.is_overdue AS IsOverdue, 
+                          c1.company_id AS CompanyId, 
+                          c1.company_name AS CompanyName
+                   FROM clients AS c
+                   INNER JOIN clients_companies AS c0 
+                   	ON c.client_id = c0.client_id
+                   INNER JOIN companies AS c1 
+                   	ON c0.company_id = c1.company_id;
+                "
+            ).AsNoTracking()
             .ToListAsync();
 
-        serviceResponse.Data = clientsCompanies.Adapt<List<GetClientCompanyViewModel>>();
+        serviceResponse.Data = clientsCompanies;
+
         return serviceResponse;
     }
 }
